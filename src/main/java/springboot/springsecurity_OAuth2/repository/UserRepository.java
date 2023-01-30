9c92d0840bf2dedd2955401d9a1f99750209f2a5
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-30 21:14:45 +0900

    feat : OAuth2 SocialLogin Repository

---
+++ src/main/java/springboot/springsecurity_OAuth2/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package springboot.springsecurity_OAuth2.repository;
+
+import org.springframework.stereotype.Repository;
+import springboot.springsecurity_OAuth2.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class UserRepository {
+
+    private Map<String, Object> users = new HashMap<>();
+
+    public User findByUsername(String username) {
+        if(users.containsKey("username")){
+            return (User) users.get(username);
+        }
+        return null;
+    }
+
+    public void register(User user) {
+        if(users.containsKey(user.getUsername())){
+            return;
+        }
+        users.put(user.getUsername(), user);
+    }
+}

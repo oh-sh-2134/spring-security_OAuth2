778395dabb383d7bbca45934766ce56d30e48c13
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-30 21:12:56 +0900

    feat : OAuth2 SocialLogin Config

--- src/main/java/springboot/springsecurity_OAuth2/OAuth2ClientConfig.java
+++ src/main/java/springboot/springsecurity_OAuth2/OAuth2ClientConfig.java
@@ -1,38 +1,73 @@
 package springboot.springsecurity_OAuth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import springboot.springsecurity_OAuth2.service.CustomOAuth2UserService;
+import springboot.springsecurity_OAuth2.service.CustomOidcUserService;
 
 @Configuration
 public class OAuth2ClientConfig {
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
+    @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
 
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-                .registrationId("keycloak")
-                .clientId("oauth2-client-app")
-                .clientSecret("gcCVBjhdUjfSN7ZNfG8S4eQNravy6bWr")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+    @Autowired
+    private CustomOidcUserService customOidcUserService;
+
+
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+//    }
+//
+//    private ClientRegistration keycloakClientRegistration() {
+//        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
+//                .registrationId("keycloak")
+//                .clientId("oauth2-client-app")
+//                .clientSecret("gcCVBjhdUjfSN7ZNfG8S4eQNravy6bWr")
+//                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
 //                .issuerUri("")
-                .build();
+//                .build();
+//    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/static/**"));
+    }
+
+
+    @Bean
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests
+                .antMatchers("/api/user")
+                .access("hasAnyRole('SCOPE_profile','SCOPE_email')")
+//                .access("hasAuthority('SCOPE_profile')")
+                .antMatchers("/api/oidc")
+                .access("hasRole('SCOPE_openid')")
+                //.access("hasAuthority('SCOPE_openid')")
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest().authenticated());
+        http.oauth2Login(oauth2 -> oauth2.userInfoEndpoint(
+                userInfoEndpointConfig -> userInfoEndpointConfig
+                        .userService(customOAuth2UserService)
+                        .oidcUserService(customOidcUserService)));
+        http.logout().logoutSuccessUrl("/");
+        return http.build();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authRequest -> authRequest.antMatchers("/loginPage").permitAll().anyRequest().authenticated());
-        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
-
-        return http.build();
+    public GrantedAuthoritiesMapper customAuthorityMapper() {
+        return new CustomAuthorityMapper();
     }
 }
